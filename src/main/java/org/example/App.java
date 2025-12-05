83c0c9bfd9321983bdcc23e4d50f2c6c5db9058a
Leeeydia <leeeydia@gmail.com>
2025-12-05 09:31:16 +0900

    파일 구조 변경

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,9 @@
 package org.example;
 
+import org.example.motivation.controller.MotivationController;
+import org.example.system.controller.SystemController;
+
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
 
 public class App {
 
@@ -15,20 +16,15 @@ public class App {
     public void run() {
         System.out.println("== motivation 실행 ==");
 
-
-//        String[][] boardArr = new String[3][];
-// List 사용 ArrayList
-
-
-        int lastId = 0;
-        List<Motivation> motivations = new ArrayList<>();
+        SystemController systemController = new SystemController();
+        MotivationController motivationController = new MotivationController(sc);
 
         while (true) {
             System.out.print("명령어 ) ");
             String cmd = sc.nextLine().trim();
 
             if (cmd.equals("exit")) {
-                System.out.println("== motivation 종료 ==");
+                systemController.exit();
                 break;
             } else if (cmd.length() == 0) {
                 System.out.println("명령어가 입력되지 않았음");
@@ -36,34 +32,14 @@ public class App {
             }
 
             if (cmd.equals("add")) {
-                System.out.print("motivation : ");
-                String motivation = sc.nextLine();
-                System.out.print("source : ");
-
-                // 객체 조립
-
-                // 객체 저장
-                //if(){}
-                // for each문
-
-                String source = sc.nextLine();
-                System.out.printf("%d번 motivation이 등록되었습니다\n", lastId);
-                lastId++;
-                count++;
-
+                motivationController.add();
             } else if (cmd.equals("list")) {
-                System.out.println("=".repeat(40));
-                System.out.printf("   번호   /    source    /    motivation   \n");
-
-                for (int i = 0; i < count; i++) {
-                    System.out.println(boardArr[i][0] + " " + boardArr[i][1] + " " + boardArr[i][2]);
-                }
+                motivationController.list();
+            } else {
+                System.out.println("사용할 수 없는 명령어야");
             }
-
-
         }
 
-    }
-// 객체 생성 Motivation
-}
 
+    }
+}
